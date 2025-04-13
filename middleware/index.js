aff5ae2f857d29291c95e6f71fbef3fd60ff9e02
TranKeTuanVuong <tuanvuongpokap@gmail.com>
2025-04-13 11:28:03 +0700

    update

--- middleware/index.js
+++ middleware/index.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 // Cấu hình multer để giới hạn dung lượng file
 const upload = multer({
   storage: multer.memoryStorage(),
-  limits: { fileSize: 50 * 1024 * 1024 }, // Giới hạn dung lượng file là 50MB
+  limits: { fileSize: 1000 * 1024 * 1024 }, // Giới hạn dung lượng file là 50MB
 });
 
 module.exports = upload;
