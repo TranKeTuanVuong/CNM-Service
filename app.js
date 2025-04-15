406bd2949a4100bf0c1dfcbb34b1da782d343535
TranKeTuanVuong <tuanvuongpokap@gmail.com>
2025-04-15 13:35:06 +0700

    update

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const ContactsRoutes = require("./routers/ContactsRouter");
 const app = express();
 
 // Middleware
-app.use(cors({ origin: ['http://localhost:5173'], methods: ["GET", "POST", "PUT", "DELETE"] }));
+app.use(cors({ origin: '*', methods: ["GET", "POST", "PUT", "DELETE"] }));
 app.use(express.json());
 
 // Kết nối MongoDB
